1cbb30c900a46ed5ec5a3d7b3deae0f0a6884592
jcssll <essentialnetworker@gmail.com>
2019-06-24 20:51:12 -0400

    Rookie mistake had quotations over query selector

--- scripts.js
+++ scripts.js
@@ -1,6 +1,19 @@
 const url = 'https://www.phila.gov/wp-json/wp/v2/posts/';
-const postsContainer = 'document.querySelector(.latestPosts)'; 
+const postsContainer = document.querySelector(".latest-post"); 
 
 fetch(url)
 .then(response => response.json())
-.then(data => console.log(data));  
+.then(data => {
+	//data brings back an array so use array method> Then provide a parameter that will be able to modify each item in the array 
+	data.map(post => {
+		const innerContent = 
+		`
+		<li>
+			<h2>${post.title.rendered}</h2>
+			<a href = "${post.link}"></a>
+		</li>
+		`
+		postsContainer.innerHTML += innerContent;
+	})
+
+}); 
